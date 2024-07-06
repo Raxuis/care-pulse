113210b33bf6aef3f53373bebdb82e8645ad8039
Raxuis <raclot.raphael@gmail.com>
2024-07-06 10:25:18 +0200

    feat(utils): :sparkles: implemented utils functions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { cn } from "@/lib/utils";
+
+const Jakarta = Plus_Jakarta_Sans(
+  {
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700"],
+    variable: "--font-sans"
+  }
+);
 
 export const metadata: Metadata = {
   title: "CarePulse",
@@ -16,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={Jakarta.className}>{children}</body>
     </html>
   );
 }
