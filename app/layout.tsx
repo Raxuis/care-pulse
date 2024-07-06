810c7dbac7c3ae433f781162002e0c84ce3eaedd
Raxuis <raclot.raphael@gmail.com>
2024-07-06 10:35:07 +0200

    feat(global): :sparkles: add shadcn with theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,13 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const Jakarta = Plus_Jakarta_Sans(
+const fontSans = Plus_Jakarta_Sans(
   {
     subsets: ["latin"],
     weight: ["300", "400", "500", "600", "700"],
-    variable: "--font-sans"
+    variable: "--font-sans",
   }
 );
 
@@ -24,7 +25,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={Jakarta.className}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
