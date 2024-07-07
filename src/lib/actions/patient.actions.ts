c076a6e26c428e5853e80391ddd510b178c59b51
Raxuis <raclot.raphael@gmail.com>
2024-07-07 20:03:05 +0200

    style(patient action): :art: improve code format

--- src/lib/actions/patient.actions.ts
+++ src/lib/actions/patient.actions.ts
@@ -41,10 +41,16 @@ export const registerPatient = async ({ identificationDocument, ...patient }: Re
   try {
     let file;
     if (identificationDocument) {
-      const inputFile = InputFile.fromBuffer(identificationDocument?.get('blobFile') as Blob, identificationDocument?.get('fileName') as string);
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get('blobFile') as Blob,
+        identificationDocument?.get('fileName') as string
+      );
 
       file = await storage.createFile(BUCKET_ID, ID.unique(), inputFile);
-      const newPatient = await databases.createDocument(DATABASE_ID, PATIENT_COLLECTION_ID, ID.unique(), {
+      const newPatient = await databases.createDocument(
+        DATABASE_ID,
+        PATIENT_COLLECTION_ID,
+        ID.unique(), {
         identificationDocument: file?.$id || null,
         identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
         ...patient
