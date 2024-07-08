cbf69eca622aadc312a8e501ec6e512462fc8d5c
Raxuis <raclot.raphael@gmail.com>
2024-07-08 12:47:44 +0200

    feat(success page): :sparkles: implemented the params to get appointment Id and appointment Infos

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,8 +1,16 @@
+import { DOCTORS } from '@/src/constants';
+import { getAppointment } from '@/src/lib/actions/appointment.action';
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Success = () => {
+const Success = async ({ params: { userId }, searchParams }: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || '';
+
+  const appointment = await getAppointment(appointmentId);
+
+  const doctor = DOCTORS.find((doctor) => doctor.name === appointment.primaryPhysician);
+
   return (
     <div className='flex h-screen max-h-screen px-[5%]'>
       <div className='success-img'>
@@ -26,6 +34,12 @@ const Success = () => {
             You have successfully <span className='text-green-500'> booked an appointment!</span>
           </h2>
           <p>We&apos;ll be in touch with you shortly to confirm.</p>
+        </section>
+        <section className='request-details'>
+          <p>Requested Appointment Details</p>
+          <div className='flex gap-3 items-center'>
+            {/* <Image  */}
+          </div>
         </section>
       </div>
     </div>
