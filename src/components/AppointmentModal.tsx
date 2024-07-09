0045c2f42d849eeeac7e29c45aee3f7e7f5cd199
Raxuis <raclot.raphael@gmail.com>
2024-07-09 13:04:13 +0200

    style(appointment modal): :lipstick: improve modal ui

--- src/components/AppointmentModal.tsx
+++ src/components/AppointmentModal.tsx
@@ -1,51 +1,56 @@
-"use client"
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import { useState } from "react"
-import { Button } from "./ui/button"
-import AppointmentForm from "./forms/AppointmentForm"
-import { Appointment } from "@/types/appwrites.types"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
+import { Appointment } from "@/types/appwrites.types";
 
-const AppointmentModal = ({
-  type,
+import { AppointmentForm } from "./forms/AppointmentForm";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+export const AppointmentModal = ({
   patientId,
   userId,
-  title,
-  description,
-  appointment
+  appointment,
+  type,
 }: {
-  type: 'schedule' | 'cancel'
-  patientId: string
-  userId: string
-  title: string
-  description: string
-  appointment?: Appointment
+  patientId: string;
+  userId: string;
+  appointment?: Appointment;
+  type: "schedule" | "cancel";
+  title: string;
+  description: string;
 }) => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+
   return (
-    <AlertDialog open={open} onOpenChange={setOpen}>
-      <AlertDialogTrigger asChild>
-        <Button variant='ghost' className={`capitalize ${type === 'schedule' && 'text-green-500'}`}>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className={`capitalize ${type === "schedule" && "text-green-500"}`}
+        >
           {type}
         </Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent className="shad-dialog sm:max-w-md">
-        <AlertDialogHeader className="mb-4 space-y-3">
-          <AlertDialogTitle className="capitalize">{type}</AlertDialogTitle>
-          <AlertDialogDescription>
-            Please fill in the following details to {type} an appointment.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
+      </DialogTrigger>
+      <DialogContent className="shad-dialog sm:max-w-md">
+        <DialogHeader className="mb-4 space-y-3">
+          <DialogTitle className="capitalize">{type} Appointment</DialogTitle>
+          <DialogDescription>
+            Please fill in the following details to {type} appointment
+          </DialogDescription>
+        </DialogHeader>
+
         <AppointmentForm
           userId={userId}
           patientId={patientId}
@@ -53,13 +58,7 @@ const AppointmentModal = ({
           appointment={appointment}
           setOpen={setOpen}
         />
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  )
-}
-
-export default AppointmentModal
+      </DialogContent>
+    </Dialog>
+  );
+};
