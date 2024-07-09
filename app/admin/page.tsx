3c12cb97b19265036877df5c39e1d76b614c022b
Raxuis <raclot.raphael@gmail.com>
2024-07-09 11:32:30 +0200

    feat(admin): :sparkles: add header

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Admin = () => {
   return (
-    <div>
-      <h1>Admin</h1>
+    <div className='mx-auto max-w-7xl px-4 flex-col space-y-14'>
+      <header className='admin-header'>
+        <Link href='/' className='cursor-pointer'>
+          <Image
+            src='/assets/icons/logo-full.svg'
+            alt='logo'
+            width={32}
+            height={162}
+            className='h-8 w-fit'
+          />
+        </Link>
+
+        <p className='text-16-semibold'>Admin Dashboard</p>
+      </header>
     </div>
   )
 }
